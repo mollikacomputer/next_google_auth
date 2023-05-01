406dd3381f1b44c6ea45a43060c0e96acaafce1c
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-05-01 18:49:06 +0600

    some added google Provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,22 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
-
+import GitHubProvider from "next-auth/providers/github";
+import FacebookProvider from "next-auth/providers/facebook";
 export default NextAuth({
   providers:[
     GoogleProvider({
       clientId:process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    })
+    }),
+    GitHubProvider({
+      clientId:process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId:process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
+
   ],
   secret:process.env.JWT_SECRET
 })
--- pages/login.js
+++ pages/login.js
@@ -1,28 +1,53 @@
-import {useSession, signIn, signOut} from 'next-auth/react';
-const login = () => {
-    const {data:session} = useSession();
-    console.log(session);
+import { useSession, signIn, signOut } from "next-auth/react";
 
-    if (session) {
-        return (
-          <div>
-            Welcome,{session.user.name}, {session.user.email},
-            <img src={session.user.image} alt="image" />
-            <button className="btn btn-success" onClick={() => signOut()}>
-              Sign Out
-            </button>
-          </div>
-        );
-      } else {
-        return (
-          <div>
-            <p> You are not signin </p>
-            <button className="btn btn-success" onClick={() => signIn()}>
-              Sign In
-            </button>
-          </div>
-        );
-      }
+const login = () => {
+  const {data:session} = useSession();
+   if(session){
+    return(
+        <div>
+            <p> Welcome, { session.user.name}, {session.user.email} </p>
+            <button className="btn" onClick={()=> signOut()} > Sign Out</button>
+        </div>
+        )
+   }else{
+    return(
+        <div>
+            <p> You are not Login </p>
+            <button className="btn" onClick={()=> signIn("google")} > LogIn with Google </button>
+            <button className="btn" onClick={()=> signIn("github")} > LogIn with Github </button>
+            <button className="btn" onClick={()=> signIn("facebook")} > LogIn with Facebook </button>
+        </div>
+    )
+   }
 };
 
 export default login;
+
+// import {useSession, signIn, signOut} from 'next-auth/react';
+// const login = () => {
+//     const {data:session} = useSession();
+//     console.log(session);
+
+//     if (session) {
+//         return (
+//           <div>
+//             Welcome,{session.user.name}, {session.user.email},
+//             <img src={session.user.image} alt="image" />
+//             <button className="btn btn-success" onClick={() => signOut()}>
+//               Sign Out
+//             </button>
+//           </div>
+//         );
+//       } else {
+//         return (
+//           <div>
+//             <p> You are not signin </p>
+//             <button className="btn btn-success" onClick={() => signIn()}>
+//               Sign In
+//             </button>
+//           </div>
+//         );
+//       }
+// };
+
+// export default login;
